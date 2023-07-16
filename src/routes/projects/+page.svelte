91eddc21182f90b909511fe66a10d8477f66cdb1
marcusmmmz <marcusizabel230@gmail.com>
2023-07-16 20:58:20 -0300

    :sparkles: feat: add images to projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,37 +1,41 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-
-	let projects = [
+	const projects = [
 		{
-			name: "Spotifds",
-			description: "A P2P music player built on IPFS",
-			url: "https://github.com/marcusmmmz/spotifds",
+			name: 'Spotifds',
+			description: 'A P2P music player built on IPFS',
+			image: 'spotifds.png',
+			url: 'https://github.com/marcusmmmz/spotifds'
 		},
 		{
-			name: "Rusty Lisp",
-			description: "A Lisp-like language made with Rust",
-			url: "https://github.com/marcusmmmz/rusty-lisp",
+			name: 'Logic-Ports',
+			description: 'A digital circuit editor made with Godot Engine.',
+			image: 'logicports.png',
+			url: 'https://github.com/marcusmmmz/Logic-Ports'
 		},
 		{
-			name: "Dsocial",
-			description: "A Discord-like Nostr client",
-			url: "https://github.com/marcusmmmz/dsocial",
+			name: 'Tetris',
+			description: 'Tetris, but made in a weekend',
+			image: 'tetris.png',
+			url: 'https://github.com/marcusmmmz/tetris'
 		},
 		{
-			name: "Logic-Ports",
-			description: "A digital circuit editor made with Godot Engine.",
-			url: "https://github.com/marcusmmmz/Logic-Ports",
+			name: 'Rusty lang',
+			description: 'A Rusty language with a compiler in Rust',
+			image: 'rustylang.png',
+			url: 'https://github.com/marcusmmmz/rusty-lang'
 		},
 		{
-			name: "Godot mod manager",
-			description: "A simple mod manager for Godot games",
-			url: "https://github.com/marcusmmmz/Godot-ModManager",
+			name: 'Dsocial',
+			description: 'A Discord-like Nostr client',
+			image: 'dsocial.png',
+			url: 'https://github.com/marcusmmmz/dsocial'
 		},
 		{
-			name: "Anki Like App",
-			description: "An app similar to Anki",
-			url: "https://github.com/marcusmmmz/anki-like-app",
-		},
+			name: 'Godot mod manager',
+			image: 'modmanager.png',
+			description: 'A simple mod manager for Godot games',
+			url: 'https://github.com/marcusmmmz/Godot-ModManager'
+		}
 	];
 </script>
 
@@ -39,11 +43,12 @@
 	<h1><a href="/about">About me</a></h1>
 	<h1>My projects:</h1>
 	<section class="projects">
-		{#each projects as { name, description, url }}
-			<div class="project" on:click={() => goto(url)}>
-				<strong>{name}</strong>
+		{#each projects as { name, description, url, image }}
+			<a class="project" href={url}>
+				<p><strong>{name}</strong></p>
+				<img src={'projects/' + image} alt="" />
 				<p>{description}</p>
-			</div>
+			</a>
 		{/each}
 	</section>
 	<h1>
@@ -53,9 +58,8 @@
 
 <style>
 	.projects {
-		/* display: flex;*/
 		display: grid;
-		grid-template-columns: 25% 25% 25%;
+		grid-template-columns: 45% 45%;
 		grid-auto-rows: auto;
 		justify-content: center;
 		gap: 2rem;
@@ -68,10 +72,21 @@
 		}
 	}
 	.project {
-		padding: 1rem;
+		display: flex;
+		flex-direction: column;
 		border-width: 4px;
 		border-style: solid;
-		cursor: pointer;
+		color: white;
+		text-decoration: none;
+		transition: transform 0.1s;
+	}
+	.project:hover {
+		transform: scale(1.05);
+	}
+	.project img {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
 	}
 	@media only screen and (max-width: 600px) {
 		.project {
